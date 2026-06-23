d3434291fbc9f04ed01d49aa657d71890ff6db06
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2026-06-23 16:38:55 +0000

    Remover página não usada

--- pages/user.tsx
+++
@@ -1,9 +0,0 @@
-import Layout from "../components/layout";
-import ProfileComponent from "../components/screens/profile";
-
-export default function ProfilePages() {
-    return <Layout
-    >
-        <ProfileComponent />
-    </Layout>
-}
